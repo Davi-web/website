ad3ace1eb9eb48b6d8a6f21b3afc6e9a67491260
David Ha <david.jeseung.ha@gmail.com>
2022-03-10 01:53:42 -0600

    Pushing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Website from "./stacks/WebsiteStack";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Website className="App" />;
 }
 
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <App />
+//     <Routes>
+//       <Route path="/" element={<Home />} />
+//       <Route path="/about" element={<About />} />
+//       <Route path="/dashboard" element={<Dashboard />} />
+//     </Routes>
+//   </BrowserRouter>,
+//   document.getElementById("root")
+// );
 export default App;
